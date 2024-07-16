c88e90ae5ed3fa4664e59eab0604fd932514199e
avhz <RustQuantContact@gmail.com>
2024-07-16 23:26:26 +0200

    INITIAL COMMIT

---
+++ app_2/app.py
@@ -0,0 +1,8 @@
+from dash import Dash, html, dcc, callback, Output, Input
+
+app = Dash()
+server = app.server
+app.layout = [html.H1(children="DASH APP: TWO", style={"textAlign": "center"})]
+
+if __name__ == "__main__":
+    app.run(debug=True)
